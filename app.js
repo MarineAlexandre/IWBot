3746948d99e18acc105e0d191d385157b2b810e8
Mariine Alexandre <marinealexandre5@gmail.com>
2017-09-15 14:25:37 +0200

    welcome and heavywork

--- app.js
+++ app.js
@@ -14,15 +14,34 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 var bot = new builder.UniversalBot(connector, function(session){
-    if(session.dialogData === null){
-        session.send(`Welcome !`);
-    }
+   //envoyer un message de bienvenue lorsqu'un utlisateur arrive sur le bot
+   bot.on('conversationUpdate', function(message){
+   		if (message.membersAdded) {
+   			message.membersAdded.forEach(function (identity){
+   				if (identity.id !== message.address.bot.id) {
+   					bot.send(new builder.Message()
+   							.address(message.address)
+   							.text("Welcome " + identity.name));
+   				}
+   			});
+   		}
+   });
+
+   //si doheavywork est écrit, attend un laps de temps avant d'envoyer une réponse
+	
+	if (session.message.text.toLowerCase() == 'doheavywork') {
+		session.sendTyping();
+		//timeout
+		setTimeout(function() {
+			session.send("Good Job !");
+		}, 5000);
+	}
 
     bot.on('typing', function(){
         session.send(`tu es en train d'écrire`);
     });
 
-    session.send(`Ok, ça fonctionne !! | [Message.length = ${session.message.text.length} ]`);
-    session.send(`Ok, ça fonctionne !! | [DialogData = ${JSON.stringify(session.dialogData)} ]`);
-    session.send(`Ok, ça fonctionne !! | [SessionState = ${JSON.stringify(session.sessionState)} ]`);
+    //session.send(`Ok, ça fonctionne !! | [Message.length = ${session.message.text.length} ]`);
+    //session.send(`Ok, ça fonctionne !! | [DialogData = ${JSON.stringify(session.dialogData)} ]`);
+    //session.send(`Ok, ça fonctionne !! | [SessionState = ${JSON.stringify(session.sessionState)} ]`);
 });
